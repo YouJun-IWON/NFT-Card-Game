3f9698a7b91013636b37e66a0d74a54e4d167fd9
ReasonJun <117330708+YouJun-IWON@users.noreply.github.com>
2023-11-30 23:15:27 +0900

    feat: :bug: fix uploadthing error

--- src/app/api/uploadthing/route.ts
+++ src/app/api/uploadthing/route.ts
@@ -5,4 +5,11 @@ import { ourFileRouter } from './core';
 // Export routes for Next App Router
 export const { GET, POST } = createNextRouteHandler({
   router: ourFileRouter,
+  config: {
+    callbackUrl: "https://nft-card-game-production.up.railway.app/api/uploadthing"
+  },
 });
+
+// export const { GET, POST } = createNextRouteHandler({
+//   router: ourFileRouter,
+// });
--- src/pages/api/socket/direct-messages/index.ts
+++ src/pages/api/socket/direct-messages/index.ts
@@ -108,9 +108,150 @@ export default async function handler(
 
     console.log('setDeck[0].length', setDeck[0].length);
 
+    if (setDeck[0].length === 0) {
+      await db.oneCardRoom.updateMany({
+        where: {
+          status: GameStatus.RUN,
+          deck1: serverId,
+        },
+        data: {
+          winner: conversation.memberOne.profileId,
+          status: GameStatus.END,
+        },
+      });
+
+      const message = await db.directMessage.create({
+        data: {
+          content,
+          fileUrl,
+          conversationId: conversationId as string,
+          memberId: member.id,
+          player1Deck: '1',
+          player2Deck: '0',
+          centerDeck: 'end',
+          showDeck: 'end',
+        },
+        include: {
+          member: {
+            include: {
+              profile: true,
+            },
+          },
+        },
+      });
+
+      await db.server.update({
+        where: {
+          id: conversation.memberTwo.serverId,
+        },
+        data: {
+          members: {
+            deleteMany: {
+              role: 'GUEST',
+            },
+          },
+          loseCount: {
+            increment: 1,
+          },
+        },
+      });
+
+      await db.server.update({
+        where: {
+          id: conversation.memberOne.serverId,
+        },
+        data: {
+          members: {
+            deleteMany: {
+              role: 'GUEST',
+            },
+          },
+          winCount: {
+            increment: 1,
+          },
+        },
+      });
+
+      const channelKey = `chat:${conversationId}:messages`;
+
+      res?.socket?.server?.io?.emit(channelKey, message);
+
+      return res.status(200).json(message);
+    } else if (setDeck[1].length === 0) {
+      await db.oneCardRoom.updateMany({
+        where: {
+          status: GameStatus.RUN,
+          deck1: serverId,
+        },
+        data: {
+          winner: conversation.memberTwo.profileId,
+          status: GameStatus.END,
+        },
+      });
+
+      const message = await db.directMessage.create({
+        data: {
+          content,
+          fileUrl,
+          conversationId: conversationId as string,
+          memberId: member.id,
+          player1Deck: '0',
+          player2Deck: '1',
+          centerDeck: 'end',
+          showDeck: 'end',
+        },
+        include: {
+          member: {
+            include: {
+              profile: true,
+            },
+          },
+        },
+      });
+
+      await db.server.update({
+        where: {
+          id: conversation.memberOne.serverId,
+        },
+        data: {
+          members: {
+            deleteMany: {
+              role: 'GUEST',
+            },
+          },
+          loseCount: {
+            increment: 1,
+          },
+        },
+      });
+
+      await db.server.update({
+        where: {
+          id: conversation.memberTwo.serverId,
+        },
+        data: {
+          members: {
+            deleteMany: {
+              role: 'GUEST',
+            },
+          },
+          winCount: {
+            increment: 1,
+          },
+        },
+      });
+
+      const channelKey = `chat:${conversationId}:messages`;
+
+      res?.socket?.server?.io?.emit(channelKey, message);
+
+      return res.status(200).json(message);
+    }
+
     // TODO: 이 부분 ADMIN 이 아닌 GUEST를 지워야 한다.
     // TODO: 그리고 Defi를 결과에 따라 처리한다. api 필요
     // TODO: 센터댁이 없을 때 남은 카드 숫자 비교
+    // TODO: 나의 덱에 한장도 없을 떄 내가 이긴게 된다. 추가하기
     if (setDeck[2].length === 0 || setDeck[0].length > 21) {
       if (
         setDeck[2].length === 0 &&
